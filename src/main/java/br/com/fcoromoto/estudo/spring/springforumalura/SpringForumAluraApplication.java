ad2ceb792c7fb748e5ed409cad9cd51548e4c77a
Francisco Coromoto <fcsouza1@stefanini.com>
2020-10-19 19:23:51 -0300

    Primeiro commit

---
+++ src/main/java/br/com/fcoromoto/estudo/spring/springforumalura/SpringForumAluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.fcoromoto.estudo.spring.springforumalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForumAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForumAluraApplication.class, args);
+	}
+
+}
