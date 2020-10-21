e76f3bb4a6ff529d7cb72a1f9650f94f7586df7b
Francisco Coromoto <fcsouza1@stefanini.com>
2020-10-21 20:09:22 -0300

    inserindo topico

--- src/main/java/br/com/fcoromoto/estudo/spring/springforumalura/controller/TopicoController.java
+++ src/main/java/br/com/fcoromoto/estudo/spring/springforumalura/controller/TopicoController.java
@@ -1,26 +1,40 @@
 package br.com.fcoromoto.estudo.spring.springforumalura.controller;
 
 import br.com.fcoromoto.estudo.spring.springforumalura.dto.TopicoDTO;
-import br.com.fcoromoto.estudo.spring.springforumalura.modelo.Curso;
+import br.com.fcoromoto.estudo.spring.springforumalura.dto.TopicoFormDTO;
 import br.com.fcoromoto.estudo.spring.springforumalura.modelo.Topico;
+import br.com.fcoromoto.estudo.spring.springforumalura.repository.CursoRepository;
 import br.com.fcoromoto.estudo.spring.springforumalura.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("topicos")
 public class TopicoController {
 
     @Autowired
-    private TopicoRepository repository;
+    private TopicoRepository topicoRepository;
 
-    @RequestMapping("topicos")
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping
     public List<TopicoDTO> listar(){
+        return TopicoDTO.fromTopicos(topicoRepository.findAll());
+    }
 
-        return TopicoDTO.fromTopicos(repository.findAll());
+    @PostMapping
+    public ResponseEntity<TopicoDTO> incluir(@RequestBody TopicoFormDTO form, UriComponentsBuilder uriBuilder){
+        Topico topico = form.convertToEntity(cursoRepository);
+        topicoRepository.save(topico);
+
+        URI uri = uriBuilder.path("topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(TopicoDTO.fromTopico(topico));
     }
 
 }
