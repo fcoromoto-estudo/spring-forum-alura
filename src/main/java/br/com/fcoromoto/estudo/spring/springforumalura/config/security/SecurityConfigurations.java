9e631e6e8e9be298e3562de33bc2de64d47744d7
Francisco Coromoto <fcsouza1@stefanini.com>
2020-10-27 20:17:33 -0300

    incluindo autorização

--- src/main/java/br/com/fcoromoto/estudo/spring/springforumalura/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/fcoromoto/estudo/spring/springforumalura/config/security/SecurityConfigurations.java
@@ -44,14 +44,15 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
             .passwordEncoder(new BCryptPasswordEncoder());
     }
 
-    @Override
     // Responsável pela autorização
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
-                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR")
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
